bf536d8a15f3c074701695974b83f19e0e0415b8
unknown <ajuarez@canteradigital.mx>
2022-11-28 18:46:45 -0800

    feat(curso): styled components

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
+import { CartIconContainer, ItemCount, ShoppingIcon } from "./cart-icon.styles";
+
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen , cartCount} = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
+  );
+};
+
+export default CartIcon;
---
+++ src/context/categories.context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useEffect, useState } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categories: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  const value = { categoriesMap };
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoryMap)
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>;
+};
